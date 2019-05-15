kdrag0n/proton_bluecross

cpu_input_boost: Convert to global display state tracker
For consistency and reduced contention on the atomic state bitfield,
switch to the global display state interface used by other code
throughout the kernel to query whether the display is on or off.

Signed-off-by: Danny Lin <danny@kdrag0n.dev>

drivers/cpufreq/cpu_input_boost.c
@@ -7,6 +7,7 @@
 
 #include <linux/cpu.h>
 #include <linux/cpufreq.h>
+#include <linux/display_state.h>
 #include <linux/input.h>
 #include <linux/kthread.h>
 #include <linux/moduleparam.h>
@@ -53,10 +54,9 @@ module_param_named(dynamic_stune_boost, stune_boost, int, 0644);
 #endif
 
 /* Available bits for boost state */
-#define SCREEN_OFF		BIT(0)
-#define INPUT_BOOST		BIT(1)
-#define MAX_BOOST		BIT(2)
-#define WAKE_BOOST		BIT(3)
+#define INPUT_BOOST		BIT(0)
+#define MAX_BOOST		BIT(1)
+#define WAKE_BOOST		BIT(2)
 
 struct boost_drv {
 	struct delayed_work input_unboost;
@@ -168,7 +168,7 @@ static void clear_stune_boost(struct boost_drv *b)
 
 static void __cpu_input_boost_kick(struct boost_drv *b)
 {
-	if (get_boost_state(b) & SCREEN_OFF)
+	if (!is_display_on())
 		return;
 
 	if (!input_boost_duration)
@@ -218,7 +218,7 @@ void cpu_input_boost_kick_max(unsigned int duration_ms)
 	if (!b)
 		return;
 
-	if (get_boost_state(b) & SCREEN_OFF)
+	if (!is_display_on())
 		return;
 
 	__cpu_input_boost_kick_max(b, duration_ms);
@@ -302,7 +302,7 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 	}
 
 	/* Unboost when the screen is off */
-	if (state & SCREEN_OFF) {
+	if (!is_display_on()) {
 		policy->min = get_min_freq(policy);
 		clear_stune_boost(b);
 		return NOTIFY_OK;
@@ -342,13 +342,10 @@ static int msm_drm_notifier_cb(struct notifier_block *nb,
 		return NOTIFY_OK;
 
 	/* Boost when the screen turns on and unboost when it turns off */
-	if (*blank == MSM_DRM_BLANK_UNBLANK) {
-		clear_boost_bit(b, SCREEN_OFF);
+	if (*blank == MSM_DRM_BLANK_UNBLANK)
 		__cpu_input_boost_kick_wake(b);
-	} else {
-		set_boost_bit(b, SCREEN_OFF);
+	else
 		wake_up(&b->boost_waitq);
-	}
 
 	return NOTIFY_OK;
 }
@@ -362,7 +359,7 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 	__cpu_input_boost_kick(b);
 
 	if (type == EV_KEY && code == KEY_POWER && value == 1 &&
-	    !(get_boost_state(b) & SCREEN_OFF))
+	    !is_display_on())
 		__cpu_input_boost_kick_wake(b);
 
 	b->last_input_jiffies = jiffies;

LaLel/lyra-4.4

Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>
Signed-off-by: Jago Gardiner (nysadev) <jagogardiner@gmail.com>

drivers/cpufreq/cpu_input_boost.c
@@ -8,6 +8,7 @@
 #include <linux/cpu.h>
 #include <linux/cpufreq.h>
 #include <linux/fb.h>
+#include <linux/display_state.h>
 #include <linux/input.h>
 #include <linux/kthread.h>
 #include <linux/moduleparam.h>
@@ -53,10 +54,9 @@ module_param_named(dynamic_stune_boost, stune_boost, int, 0644);
 #endif
 
 /* Available bits for boost state */
-#define SCREEN_OFF		BIT(0)
-#define INPUT_BOOST		BIT(1)
-#define MAX_BOOST		BIT(2)
-#define WAKE_BOOST		BIT(3)
+#define INPUT_BOOST		BIT(0)
+#define MAX_BOOST		BIT(1)
+#define WAKE_BOOST		BIT(2)
 
 struct boost_drv {
 	struct delayed_work input_unboost;
@@ -168,7 +168,7 @@ static void clear_stune_boost(struct boost_drv *b)
 
 static void __cpu_input_boost_kick(struct boost_drv *b)
 {
-	if (get_boost_state(b) & SCREEN_OFF)
+	if (!is_display_on())
 		return;
 
 	if (!input_boost_duration)
@@ -218,7 +218,7 @@ void cpu_input_boost_kick_max(unsigned int duration_ms)
 	if (!b)
 		return;
 
-	if (get_boost_state(b) & SCREEN_OFF)
+	if (!is_display_on())
 		return;
 
 	__cpu_input_boost_kick_max(b, duration_ms);
@@ -302,7 +302,7 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 	}
 
 	/* Unboost when the screen is off */
-	if (state & SCREEN_OFF) {
+	if (!is_display_on()) {
 		policy->min = get_min_freq(policy);
 		clear_stune_boost(b);
 		return NOTIFY_OK;
@@ -342,13 +342,10 @@ static int fb_notifier_cb(struct notifier_block *nb,
 		return NOTIFY_OK;
 
 	/* Boost when the screen turns on and unboost when it turns off */
-	if (*blank == FB_BLANK_UNBLANK) {
-		clear_boost_bit(b, SCREEN_OFF);
+	if (*blank == FB_BLANK_UNBLANK)
 		__cpu_input_boost_kick_wake(b);
-	} else {
-		set_boost_bit(b, SCREEN_OFF);
+	else
 		wake_up(&b->boost_waitq);
-	}
 
 	return NOTIFY_OK;
 }
@@ -362,7 +359,7 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 	__cpu_input_boost_kick(b);
 
 	if (type == EV_KEY && code == KEY_POWER && value == 1 &&
-	    !(get_boost_state(b) & SCREEN_OFF))
+	    !is_display_on())
 		__cpu_input_boost_kick_wake(b);
 
 	b->last_input_jiffies = jiffies;